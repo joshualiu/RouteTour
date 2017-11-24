5fd7181baf1f0e25a2b462c1f1b54dd6fe727c8f
Dishant <dishantpatel27@gmail.com>
2017-11-24 17:23:32 +0000

    added google custom search api

--- app/chat/index.js
+++ app/chat/index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var $ = require('jquery');
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 var fs = require('fs');
@@ -29,9 +30,10 @@ io.on('connection', function(socket){
            console.log('error:', err);
          else
            io.emit('chat message',JSON.stringify(response, null, 2));
-           console.log(JSON.stringify(response, null, 2));
+           //console.log(searchFor("toronto"));
+           //console.log(JSON.stringify(response, null, 2));
      });
-
+  
   });
 });
 
@@ -65,3 +67,60 @@ http.listen(port, function(){
 //     }
 //   ]
 // }
+
+
+/**
+ * Use Google's customsearch API to perform a search query.
+ * See https://developers.google.com/custom-search/json-api/v1/using_rest.
+ *
+ * param {string} query   Search query to perform, e.g. "test"
+ *
+ * returns {object}        See response data structure at
+ *                         https://developers.google.com/custom-search/json-api/v1/reference/cse/list#response
+ */
+// $(document).ready(function() {( query="toronto" ) => {
+  
+//     // Base URL to access customsearch
+//     var urlTemplate = "https://www.googleapis.com/customsearch/v1?key=%KEY%&cx=%CX%&q=%Q%";
+  
+//     // Script-specific credentials & search engine
+//     var ApiKey = "AIzaSyDe80pVuP-mC6NQtMttxsmBta0J3HXrvLM";
+//     var searchEngineID = "001821595967333671065:nb59ve0r6gs";
+  
+//     // Build custom url
+//     var url = urlTemplate
+//       .replace("%KEY%", encodeURIComponent(ApiKey))
+//       .replace("%CX%", encodeURIComponent(searchEngineID))
+//       .replace("%Q%", encodeURIComponent(query));
+  
+//     var params = {
+//       muteHttpExceptions: true
+//     };
+  
+//     $.ajax({
+//       url: url,
+//       type: 'GET',
+//       success: (result) =>{
+//         console.log(result);
+//       }
+//     })
+  
+//     // Perform search
+//     //Logger.log( UrlFetchApp.getRequest(url, params) );  // Log query to be sent
+   
+   
+//     // var response = UrlFetchApp.fetch(url, params);
+//     // var respCode = response.getResponseCode();
+  
+//     // if (respCode !== 200) {
+//     //   throw new Error ("Error " +respCode + " " + response.getContentText());
+//     // }
+//     // else {
+//     //   // Successful search, log & return results
+//     //   var result = JSON.parse(response.getContentText());
+//     //   Logger.log( "Obtained %s search results in %s seconds.",
+//     //              result.searchInformation.formattedTotalResults,
+//     //              result.searchInformation.formattedSearchTime);
+//     //   return result;
+//    // }
+//   })}
